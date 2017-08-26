9011877fcb6b1f952d4dd01b5e9f42df4089c3d6
chenyangjun <454857757@qq.com>
2017-08-26 21:12:48 +0800

    360笔试题

---
+++ Offer/src/com/cyj/school_problem_2017/a20170826_360_交换两次数字_Main.java
@@ -0,0 +1,60 @@
+package com.cyj.school_problem_2017;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class a20170826_360_交换两次数字_Main {
+/*
+5 5
+1 2 3 3
+2 1 3
+
+
+5 15
+1 2 3 6
+2 1 3
+
+
+*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()){
+			int n=sc.nextInt();//彩色
+			int m=sc.nextInt();//白色
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int c=sc.nextInt();
+			int d=sc.nextInt();
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			int z=sc.nextInt();
+			//每种最大的个数
+			int max1 = Math.min(n/a, m/b);
+			int max2 = m/c;
+			int max3 = n/d;
+			ArrayList<Integer> result = new ArrayList<Integer>();
+			//枚举所有结果
+			int curBig = 0;
+			for(int i=0;i<=max1;i++){
+				for(int j=0;j<=max2;j++){
+					for(int x1=0;x1<=max3;x1++){
+						if(((i*b+j*c)<=n)
+								&&(i*a+x1*d)<=m){
+							int curResult = x*i+y*j+z*x1;
+							//result.add(curResult);
+							if(curResult>=curBig){
+								curBig=curResult;
+							}
+						}
+					}
+				}
+			}
+			//Collections.sort(result);
+			System.out.println(curBig);
+		}
+	}
+
+}
---
+++ Offer/src/com/cyj/school_problem_2017/a20170826_360_跳水运动员_Main.java
@@ -0,0 +1,75 @@
+package com.cyj.school_problem_2017;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class a20170826_360_跳水运动员_Main {
+
+/*
+4
+1 3 7 9
+3
+2 10 12
+
+*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()){
+			int na = sc.nextInt();
+			int arrA[] = new int[na];
+			for(int i=0;i<na;i++){
+				arrA[i] = sc.nextInt();
+				//System.out.print(arrA[i]+" ");
+			}
+			//System.out.println();
+			int nb = sc.nextInt();
+			int arrB[] = new int[nb];
+			for(int i=0;i<nb;i++){
+				arrB[i] = sc.nextInt();
+				//System.out.print(arrA[i]+" ");
+			}
+			
+			//System.out.println();
+			//boolean calArr[][] = new boolean [na][nb];
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			int sumA=0;
+			int sumB=0;
+			for(int i=0;i<na;i++){
+				sumA = sumA+arrA[i];
+			}
+			for(int i=0;i<nb;i++){
+				sumB = sumB+arrB[i];
+			}
+			for(int i=0;i<na;i++){
+				for(int j=0;j<nb;j++){
+					//第一次交换
+					swap(arrA,arrB,i,j);
+					sumA = sumA-arrA[i]+arrB[j];
+					sumB = sumB+arrA[i]-arrB[j];
+					//calArr[i][j]=true;
+					for(int x=0;x<na;x++){
+						for(int y=0;y<nb;y++){
+							if(x==i&&y==j){
+								continue;
+							}
+							swap(arrA,arrB,x,y);
+							sumA = sumA-arrA[x]+arrB[y];
+							sumB = sumB+arrA[x]-arrB[y];
+							list.add((int)Math.abs(sumA-sumB));
+						}
+					}
+				}
+			}
+			Collections.sort(list);
+			System.out.println(list.get(0));
+		}
+		
+	}
+	public static void swap(int []arrA,int[] arrB,int i,int j){
+		int tmp =  arrA[i];
+		arrA[i] = arrB[j];
+		arrB[j] = tmp;
+	}
+}
