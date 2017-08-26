c8091930ce4679e621ea431548d12fbd12cccb3b
chenyangjun <454857757@qq.com>
2017-08-26 16:50:48 +0800

    aaa

---
+++ Offer/src/com/cyj/tmp3/Main2.java
@@ -0,0 +1,12 @@
+package com.cyj.tmp3;
+
+import java.util.Scanner;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	}
+	
+}
